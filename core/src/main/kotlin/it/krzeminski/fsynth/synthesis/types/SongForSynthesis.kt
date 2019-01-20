e9c3e9ddc6818425b450956efee8fc9c3b3a2ffe
Piotr Krzeminski <git@krzeminski.it>
2019-01-20 22:15:07 +0100

    Remove code from fsynth and configure build system
    This commit is a minimal set of changes to make PlotAssert
    build and to not have leftover files after moving git history from krzema12/fsynth.

--- core/src/main/kotlin/it/krzeminski/fsynth/synthesis/types/SongForSynthesis.kt
+++
@@ -1,9 +0,0 @@
-package it.krzeminski.fsynth.synthesis.types
-
-import it.krzeminski.fsynth.types.Waveform
-
-data class SongForSynthesis(val tracks: List<TrackForSynthesis>)
-
-data class TrackForSynthesis(val segments: List<TrackSegmentForSynthesis>, val volume: Float)
-
-data class TrackSegmentForSynthesis(val waveform: Waveform, val durationInSeconds: Float)
