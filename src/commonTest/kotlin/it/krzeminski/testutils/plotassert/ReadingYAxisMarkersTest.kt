e193fef08a9fbcc2354be53f42047ef15a713738
Piotr Krzeminski <git@krzeminski.it>
2019-05-18 09:22:24 +0200

    Remove 'fail(...)' used in 'assertFailsWith(...)'
    If the function called in `assertFailsWith(...)` doesn't throw
    an exception, this assert function fails on its own. There's no
    need to add an explicit `fail(...)`.

--- src/commonTest/kotlin/it/krzeminski/testutils/plotassert/ReadingYAxisMarkersTest.kt
+++ src/commonTest/kotlin/it/krzeminski/testutils/plotassert/ReadingYAxisMarkersTest.kt
@@ -8,7 +8,6 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
-import kotlin.test.fail
 
 class ReadingYAxisMarkersTest {
     @Test
@@ -50,7 +49,6 @@ class ReadingYAxisMarkersTest {
                                     values = listOf(0.0f, 1.0f))
                     )
             )
-            fail("It should throw ${IllegalArgumentException::class}!")
         }.let { e ->
             assertTrue(e.message in setOf(
                     "Given Y axis markers should have descending values (found: 1.0, 1.0)!",
@@ -73,7 +71,6 @@ class ReadingYAxisMarkersTest {
                                     values = listOf(0.0f, 1.0f))
                     )
             )
-            fail("It should throw ${IllegalArgumentException::class}!")
         }.let { e ->
             assertTrue(e.message in setOf(
                     "Given Y axis markers should have descending values (found: 4.0, 9.0)!",
@@ -95,7 +92,6 @@ class ReadingYAxisMarkersTest {
                                     values = listOf(0.0f, 1.0f))
                     )
             )
-            fail("It should throw ${IllegalArgumentException::class}!")
         }.let { e ->
             assertEquals("0 Y axis marker(s) found, and there should be at least two!", e.message)
         }
@@ -115,7 +111,6 @@ class ReadingYAxisMarkersTest {
                                     values = listOf(0.0f, 1.0f))
                     )
             )
-            fail("It should throw ${IllegalArgumentException::class}!")
         }.let { e ->
             assertEquals("1 Y axis marker(s) found, and there should be at least two!", e.message)
         }
